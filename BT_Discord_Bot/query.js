46184c4969ca66906fbf6a625573d44bbf50853a
Dylan Randall <dylan.d.randall@gmail.com>
2017-11-22 23:41:23 +1100

    update

--- BT_Discord_Bot/query.js
+++ BT_Discord_Bot/query.js
@@ -1,12 +1,17 @@
-﻿module.exports.query = function(statement)
+﻿const host = process.argv[1];
+const user = process.argv[2];
+const password = process.argv[3];
+const database = process.argv[4];
+
+module.exports.query = function (statement)
 {
     return new Promise(function(resolve, reject) {
         let mysql = require('mysql');
         let conn = mysql.createConnection({
-            host: '<host>',
-            user: '<username>',
-            password: '<password>',
-            database: '<database>',
+            host: host,
+            user: user,
+            password: password,
+            database: database,
             supportBigNumbers: true
         });
 
